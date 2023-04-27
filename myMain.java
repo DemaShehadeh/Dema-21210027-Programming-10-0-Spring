916004d28f406fbc150825a9aadc0bac7e6eff12
DemaShehadeh <110935236+DemaShehadeh@users.noreply.github.com>
2023-04-27 12:18:40 +0300

    Create myMain.java

---
+++ myMain.java
@@ -0,0 +1,12 @@
+package package1;
+
+public class myMain {
+
+	public static void main(String[] args) {
+		Students student1 = new Students();
+		Students student2 = new Students();
+		Students student3 = new Students();
+
+	}
+
+}
